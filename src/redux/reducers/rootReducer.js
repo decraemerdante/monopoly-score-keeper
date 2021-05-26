04bf305f14c3cc7c1b2c84407f28a6e5f07b5774
decraemerdante@gmail.com <decraemerdante@gmail.com>
2021-05-26 12:41:35 +0200

    Updates

--- src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import playerReducer from "./implementations/playerReducer";
-
-const rootReducer = combineReducers({
-  players: playerReducer,
-});
-
-export default rootReducer;
